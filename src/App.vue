e45e16f3933ffdb622036e4e043bb005f7c47eb9
fanmiao11 <miao623@outlook.com>
2022-07-06 09:22:26 +0800

    注册并引入组件

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MyHeader ></MyHeader>
+    <my-goods></my-goods>
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue';
+import MyGoods from './components/MyGoods.vue';
+import MyFooter from './components/MyFooter.vue';
+
+export default {
+  components: {
+    MyHeader,
+    MyGoods,
+    MyFooter,
+  },
+};
+</script>
+
+<style></style>
